8ac837f198b4a04c8f08fdf24820002cb41e222a
Lewis Nixon <lewcfn@gmail.com>
2023-08-15 15:52:17 +0100

    Add a submit handler, log form output to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import DeployPage from './DeployPage';
-import { PRICING, PRODUCTS, REGIONS } from './data';
+import DeployPage from './views/DeployPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -10,11 +9,7 @@ function App() {
             <Header />
             <main className="bg-neutral-100 flex-grow px-6 py-4">
                 <h1 className="font-bold font-lg mb-6">Cluster Configuration</h1>
-                <DeployPage
-                    regions={REGIONS}
-                    products={PRODUCTS}
-                    pricing={PRICING}
-                />
+                <DeployPage />
             </main>
             <Footer />
         </div>
